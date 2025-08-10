308be75370a0a4c66731b4a4c71d385e6b1b0f17
Hai Vo <haigunshipbattle@gmail.com>
2025-08-10 11:36:25 +0700

    Initial commit

---
+++ services/blog/src/utils/consumer.ts
@@ -0,0 +1,83 @@
+import amqp from "amqplib";
+import { redisClient } from "../server.js";
+import { sql } from "./db.js";
+
+interface CacheInvalidationMessage {
+    action: string,
+    keys: string[],
+};
+
+export const startCacheConsumer = async () => {
+    try {
+        const connection = await amqp.connect({
+            protocol: 'amqp',
+            hostname: process.env.Rabbimq_Host,
+            port: 5672,
+            username: process.env.Rabbimq_Username,
+            password: process.env.Rabbimq_Password,
+        });
+
+        const channel = await connection.createChannel();
+
+        const queueName = "cache-invalidation";
+
+        await channel.assertQueue(queueName, { durable: true });
+
+        console.log("✅ Blog service cache consumer start");
+
+        channel.consume(queueName, async (msg) => {
+            if (msg) {
+
+                try {
+                    const content = JSON.parse(
+                        msg.content.toString()
+                    ) as CacheInvalidationMessage;
+
+                    console.log("̃📩 Blog service received cache invalidation message",
+                        content
+                    );
+
+                    if (content.action === "invalidateCache") {
+                        for (const pattern of content.keys) {
+                            const keys = await redisClient.keys(pattern);
+
+                            if (keys.length > 0) {
+                                await redisClient.del(keys);
+
+                                console.log(
+                                    `🗑 Blog service invalidated ${keys.length} cache keys matching ${pattern}`
+                                );
+
+                                const category = "";
+
+                                const searchQuery = "";
+
+                                const cacheKey = `blog:${searchQuery}:${category}`;
+
+                                const blogs = 
+                                await sql`SELECT * FROM blogs ORDER BY created_at DESC`;
+
+
+                                await redisClient.set(cacheKey, JSON.stringify(blogs),{
+                                    EX:3600,
+                                });
+                                console.log("🔄 Cache rebuilt with key:", cacheKey);
+                            }
+                        }
+                    }
+
+                    channel.ack(msg);
+                } catch (error) {
+                    console.error(
+                        "❌ Error processing cache invalidation in blog service:",
+                         error
+                        );
+
+                        channel.nack(msg,false,true);
+                }
+            }
+        })
+    } catch (error) {
+        console.error("❌ Failed to start rabbitmq consumer ");
+    }
+}
